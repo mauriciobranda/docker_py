4ff3102d3ac8a3667b355fec307c573744823823
mauriciobranda <mauricio.20@gmail.com>
2021-06-12 16:13:18 -0300

    somente maiores de 2000

--- main.py
+++ main.py
@@ -17,33 +17,35 @@ def main():
     movietags = soup.select('td.titleColumn')
     inner_movietags = soup.select('td.titleColumn a')
     ratingtags = soup.select('td.posterColumn span[name=ir]')
-
+    
     def get_year(movie_tag):
         moviesplit = movie_tag.text.split()
         year = moviesplit[-1] # last item 
-        print(str(year))
+        x = year.replace(")", "")
+        yearT = x.replace("(", "")
 
+        yearF=0
+        if int(yearT) > 1950:
+            yearF = yearT
 
-
-        
-        return year
+        return yearF
 
     years = [get_year(tag) for tag in movietags]
-    
+
     actors_list =[tag['title'] for tag in inner_movietags] # access attribute 'title'
     titles = [tag.text for tag in inner_movietags]
     ratings = [float(tag['data-value']) for tag in ratingtags] # access attribute 'data-value'
-    print("Here I am 2")
     n_movies = len(titles)
 
     while(True):
         idx = random.randrange(0, n_movies)
         
-        print(f'{titles[idx]} {years[idx]}, Rating: {ratings[idx]:.1f}, Starring: {actors_list[idx]}')
+        if int(years[idx]) > 2000:
+            print(f'{titles[idx]} {years[idx]}, Rating: {ratings[idx]:.1f}, Starring: {actors_list[idx]}')
 
-        user_input = input('Do you want another movie (y/[n])? ')
-        if user_input != 'y':
-            break
+            user_input = input('Do you want another movie (y/[n])? ')
+            if user_input != 'y':
+                break
     
 
 if __name__ == '__main__':
